ee1e0c1fbaaa76ab8914f92e48a42adbfef9e6d3
aakash2809 <aakashrajak289@gmail.com>
2020-12-18 07:16:14 +0530

    Add : User Email Id validation by regex

--- UserRegistrationValidator.js
+++ UserRegistrationValidator.js
@@ -1,6 +1,6 @@
 isValidFirstName = (fName) => {
-    let pattern = new RegExp(`^[A-Z]{1}[a-zA-Z]{2,}$`);
-    if (pattern.test(fName)) {
+    let FirstNamePattern = new RegExp(`^[A-Z]{1}[a-zA-Z]{2,}$`);
+    if (FirstNamePattern.test(fName)) {
         console.log(`${fName} is Valid first name!`);
     }
     else {
@@ -8,6 +8,7 @@ isValidFirstName = (fName) => {
     }
 }
 
+// Last name validator fucntion
 isValidLastName = (lName) => {
     let pattern = new RegExp(`^[A-Z]{1}[a-zA-Z]{2,}$`);
     if (pattern.test(lName)) {
@@ -18,11 +19,25 @@ isValidLastName = (lName) => {
     }
 }
 
+// Email validator fucntion
+isValidEmail = (email) => {
+    let emailPattern = new RegExp(`^([A-Za-z\\d-_\\+]+)(\\.[A-Za-z\\d-_]+)?@([a-zA-Z\\d]+)\\.([a-zA-Z]{2,4})(\\.[A-Za-z]{2,4})?$`);
+    if (emailPattern.test(email)) {
+        console.log(`${email} is a Valid email id!`);
+    }
+    else {
+        console.log(`${email} is an InValid email id!`);
+    }
+}
+
 console.log(`\nWelcome to User Registration Program`);
 isValidFirstName(`Raj`);
 isValidFirstName(`Ra`);
 isValidFirstName(`raj`);
 
-isValidLastName(`Patil`); 
-isValidLastName(`patil`); 
+isValidLastName(`Patil`);
+isValidLastName(`patil`);
 isValidLastName(`pat`);
+
+isValidEmail('abc@yahoo.com');
+isValidEmail('abc');
