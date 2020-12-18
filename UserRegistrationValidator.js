011a494754a96fad5c08c1219925cf98395f96a0
aakash2809 <aakashrajak289@gmail.com>
2020-12-18 06:26:42 +0530

    Add : user first name validation by regex

--- UserRegistrationValidator.js
+++ UserRegistrationValidator.js
@@ -1,1 +1,14 @@
+isValidFirstName = (fName) => {
+    let pattern = new RegExp(`^[A-Z]{1}[a-zA-Z]{2,}$`);
+    if (pattern.test(fName)) {
+        console.log(`${fName} is Valid first name!`);
+    }
+    else {
+        console.log(`${fName} is InValid first name!`);
+    }
+}
+
 console.log(`\nWelcome to User Registration Program`);
+isValidFirstName("Raj");
+isValidFirstName("Ra");
+isValidFirstName("raj");
