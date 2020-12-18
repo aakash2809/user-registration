1d905d0966c02c72d2b1c2022090894bcae437b7
aakash2809 <aakashrajak289@gmail.com>
2020-12-18 06:43:53 +0530

    Add : User last name validation by regex

--- UserRegistrationValidator.js
+++ UserRegistrationValidator.js
@@ -8,7 +8,21 @@ isValidFirstName = (fName) => {
     }
 }
 
+isValidLastName = (lName) => {
+    let pattern = new RegExp(`^[A-Z]{1}[a-zA-Z]{2,}$`);
+    if (pattern.test(lName)) {
+        console.log(`${lName} is Valid last name!`);
+    }
+    else {
+        console.log(`${lName} is InValid last name!`);
+    }
+}
+
 console.log(`\nWelcome to User Registration Program`);
-isValidFirstName("Raj");
-isValidFirstName("Ra");
-isValidFirstName("raj");
+isValidFirstName(`Raj`);
+isValidFirstName(`Ra`);
+isValidFirstName(`raj`);
+
+isValidLastName(`Patil`); 
+isValidLastName(`patil`); 
+isValidLastName(`pat`);
